ba9185bbc6c09eea85413259bbdcf47fac1ef140
none <none@ee.ee>
2020-09-23 10:49:51 +0300

    дополненное задание

--- App.java
+++ App.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package perevertish;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -23,5 +25,22 @@ class App {
         }
         String rStr = new String(rChStr);
         System.out.println("Перевёртыш: "+rStr);
+        StringBuilder sbStr = new StringBuilder(str);
+        System.out.println("Перевёртыш 2: "+sbStr.reverse().toString());
+        System.out.println("Второе слово в строке: ");
+        int firstSpace = str.indexOf(" ");
+        String str2 = str.substring(firstSpace+1);
+        int nextSpace = str2.indexOf(" ");
+        String secondWord = "";
+        if(firstSpace < 0){
+            System.out.println("Второго слова нет");
+        }
+        else if(nextSpace > 0){
+            secondWord = str.substring(firstSpace+1, nextSpace+firstSpace+1);
+        }else{
+            secondWord = str2;
+        }
+        System.out.println(secondWord);
+        
     }
 }
